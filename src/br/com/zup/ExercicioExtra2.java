562d027dbd6752544c697d7ba3ff406f109f2491
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-01 08:42:40 -0300

    Resolução Exercicio Extra 2  da Lista 6

---
+++ src/br/com/zup/ExercicioExtra2.java
@@ -0,0 +1,24 @@
+package br.com.zup;
+
+public class ExercicioExtra2 {
+
+    public static void main(String[] args) {
+
+        //criando variaveis
+        int contador;
+        int somaNumerosImpares = 0;
+
+        for (contador = 0; contador <= 50; contador++){
+
+            if (contador % 2 != 0){
+                somaNumerosImpares += contador;
+
+                System.out.println(somaNumerosImpares);
+            }
+        }
+
+        System.out.println("A soma de todos os números impares de 0  a 50 é " + somaNumerosImpares);
+
+    }
+
+}
